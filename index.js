763ea12c779a8207d049e7e5c303958b4ff02b46
Amit Kanfer <amit@fire.glass>
2016-05-25 10:45:55 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 var peek = require('bindings')('peek.node').peek;
 
 module.exports = function(fd, size, argp) {
-    var ret = peek(fd, size, argp);
-    if (ret < 0) {
-        var e = new Error('peek ' + ret);
-        e.errno = e.code = ret;
-        e.syscall = peek;
-        throw e;
-    }
-
-    return ret;
+    return peek(fd, size, argp);
 };
